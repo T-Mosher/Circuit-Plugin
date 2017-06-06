53acbf000c6829a7b1ab0ae5c67f4f41c336bb9e
Tom Mosher <ab7nv@yahoo.com>
2017-06-05 23:30:12 -0700

    Added event and entry utility functions, updated custom_events to use them.
    Added functions to functions.php
    Modified custom_events template to use the utilities.
    Updated custom_events to display entries and capacity.

--- BandChild/functions.php
+++ BandChild/functions.php
@@ -3,14 +3,70 @@ function nwapaGetUnitInfo(string $id) {
 	echo '<br>Testing the function<br>';
 
 }
+
+function nwapaGetActiveEvents() {
+	// this function returns an array of all active events in chronological order
+	global $wpdb;
+	
+	$events = array();
+	$query = "SELECT * FROM events WHERE event_active = 'T' ORDER BY event_date ASC";
+	$response = $wpdb->get_results($query, ARRAY_A);
+	if(!empty($response)) {
+		foreach($response as $row) {
+			// push this event onto the array
+			$events[] = $row;
+		}
+	} else {
+		echo '<br>No active events found.<br>';
+		$wpdb->print_error();
+	}
+	return $events;
+}
+
+function nwapaGetEntryCounts() {
+	// this function returns an assoc array giving the number of entries in each active event
+	// for each row in the return array:
+	//    'event_id' is the event number
+	//    'cnt' is the number of entries in that event
+	global $wpdb;
+	
+	$events = nwapaGetActiveEvents();
+	$entries = array();
+	foreach ($events as $e) {
+		// Debug
+		//echo '<br>Testing event:'.$e['event_id'].' Name: '.$e['event_name'].'<br>';
+		
+		$query = "SELECT * FROM entries WHERE what_event = ".$e['event_id'];
+		$entries_resp = $wpdb->get_results($query, ARRAY_A);
+		$cnt = 0;
+		if (!empty($entries_resp)) {
+			
+			foreach($entries_resp as $entry) {
+				if ($e['event_id'] ===  $entry['what_event']) {
+					$cnt = $cnt + 1;
+				}
+			}
+			// Debug
+			//echo '<br>Entries for this event: '.$cnt.'<br>';
+		} else {
+			// Debug
+			//echo '<br>No entries in this event.<br>';
+		}
+		$entries[$e['event_id']] = $cnt;
+	}
+	// Debug
+	//var_disp($entries);
+	return $entries;
+}
+
 function var_disp($var) {
 	// this function puts some HTML framing around var_dump()'s output
-	
 	echo '<br>';
 	var_dump($var);
 	echo '<br>';
 }
 
+// Note: need to update this function name
 function get_nwapa_settings($param) {
 /*
 * Read a parameter $param from the nwapa_settings table
--- BandChild/templates/custom_events.php
+++ BandChild/templates/custom_events.php
@@ -7,51 +7,51 @@ Template Name: Custom Events
 <?php
 global $wpdb;
 
-get_header();
+	get_header();
 
-echo '<br>';
-echo '<h3>This page displays all scheduled events.</h3>';
-echo '<p>To enter an event, use your unit registration link.</p>';
-echo '<p>To view the event and entry list, click on the Event Name link.</p>';
-echo '<br>';
+	echo '<br>';
+	echo '<h3>This page displays all scheduled events.</h3>';
+	echo '<p>To enter an event, use your unit registration link.</p>';
+	echo '<p>To view the event and entry list, click on the Event Name link.</p>';
+	echo '<br>';
 
-$query = "SELECT * FROM events WHERE event_active = 'T' ORDER BY event_date ASC";
+	$events = nwapaGetActiveEvents();
 
-$response = $wpdb->get_results($query, ARRAY_A);
- 
-if($response) {
-	echo '<table style=bold>
+	echo '<table style=bold cellpadding=3>
 		<tr>
 		<td><b>Date</b></td>
 		<td><b>Event Name</b></td>
 		<td><b>Location</b></td>
-		<td><b>Host</b></td>
 		<td><b>Event Type</b></td>
+		<td><b>Entries</b></td>
+		<td><b>Capacity</b></td>
 		</tr>';
 	
-	foreach($response as $row) {
-		// read the number of entries for each event
-		//$event_entries = count($row['event_id']);
-		$event_entries = 99;
-		
+	// get the count of entries in each event	
+	$event_entries = nwapaGetEntryCounts();
+	// Debug
+	//var_disp($event_entries);
+	
+	foreach($events as $row) {
 		// populate each row of the event summary table
 		echo '<tr>';
+		//echo '<td>' . $row['event_date'] .' id:'.$row['event_id'] . '</td>';
 		echo '<td>' . $row['event_date'] . '</td>';
 		echo '<td>' . '<a href="'.$row['event_link'].'">'.$row['event_name'] . '</a></td>';
 		echo '<td>' . $row['event_location'] . '</td>';
-		echo '<td>' . $row['event_host'] . '</td>';
-		//echo '<td>' . $row['event_capacity'] . '</td>';
-		//echo '<td>' . $event_entries . '</td>';
-		echo '<td>' . $row['event_type'] . '</td>';
+		echo '<td>' . $row['event_type']. '</td>';
+		//echo '<td>--</td>';
+		echo '<td>' . $event_entries[$row['event_id']].'</td>';
+		echo '<td>' . $row['event_capacity'] . '</td>';
  		echo '</tr>';
 	}
 	echo '</table>';
 	echo '<br><br>';
-} else {
-	echo "Couldn't issue database query<br><br>";
+//} else {
+//	echo "Couldn't issue database query<br><br>";
 	//print_r($response);
-	$wpdb->print_error();
-}
+//	$wpdb->print_error();
+//}
 
 get_footer();
 ?>
